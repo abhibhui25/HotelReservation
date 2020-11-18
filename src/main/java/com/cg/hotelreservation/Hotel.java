5b4a6a4d4b216190afb2e46b0ceb093a40d224a9
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-11-18 21:57:53 +0530

    complete Hotel Reservation program

---
+++ src/main/java/com/cg/hotelreservation/Hotel.java
@@ -0,0 +1,68 @@
+package com.cg.hotelreservation;
+
+public class Hotel {
+    public String hotelName;
+    public double rate;
+    public String startDate;
+    public String endDate;
+    public double weekdayRate;
+    public double weekendRate;
+    public int rating;
+    public double specialWeekdayRate;
+    public double specialWeekendRate;
+    public String customerType = "Regular";
+
+    public Hotel(String hotelName, int weekdayRate) {
+        this.hotelName = hotelName;
+        this.rate = rate;
+        this.startDate = " ";
+        this.endDate = " ";
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = 0;
+        this.rating = 0;
+    }
+    public Hotel(String hotelName, double weekdayRate, String startDate, String endDate){
+        this.weekendRate = 0;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rating = 0;
+    }
+
+    public Hotel(String hotelName, double weekdayRate, double weekendRate, String startDate, String endDate, int rating) {
+        this.hotelName = hotelName;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rating = rating;
+    }
+    public Hotel(String hotelName, double weekdayRate, double weekendRate, String startDate, String endDate){
+        this.weekendRate = weekendRate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rating = 0;
+    }
+    public Hotel(String hotelName, double weekdayRate, double weekendRate, String startDate, String endDate, int rating, double specialWeekdayRate, double specialWeekendRate, String customerType) {
+        this.hotelName = hotelName;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rating = rating;
+        this.specialWeekdayRate = specialWeekdayRate;
+        this.specialWeekendRate = specialWeekendRate;
+        this.customerType = customerType;
+    }
+    public boolean validate() {
+        HotelReservation hotelReservationMain = new HotelReservation();
+        try {
+            return hotelReservationMain.validateDate(this.startDate) &&
+                    hotelReservationMain.validateDate(this.endDate) &&
+                    hotelReservationMain.validateCustomerType(customerType);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+}
+
---
+++ src/main/java/com/cg/hotelreservation/HotelReservation.java
@@ -0,0 +1,74 @@
+package com.cg.hotelreservation;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class HotelReservation {
+    public ArrayList<Hotel> hotelList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+
+    public Hotel findCheapestHotelByWeekdayRates(String startDate, String endDate) {
+        Optional<Hotel> cheapestHotel = hotelList.stream()
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .reduce((hotel1, hotel2) -> hotel1.weekdayRate < hotel2.weekdayRate ? hotel1 : hotel2);
+        if (cheapestHotel.isPresent())
+            return cheapestHotel.get();
+        return null;
+    }
+
+    public Hotel findCheapestHotelByWeekendRates(String startDate, String endDate) {
+        Optional<Hotel> cheapestHotel = hotelList.stream()
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .reduce((hotel1, hotel2) -> hotel1.weekendRate < hotel2.weekendRate ? hotel1 : hotel2);
+        if (cheapestHotel.isPresent())
+            return cheapestHotel.get();
+        return null;
+    }
+
+    public Hotel findCheapestBestRatedHotelByWeekdayRates(String startDate, String endDate) {
+        double bestRating = hotelList.stream()
+                .filter(h -> h.validate())
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .reduce((hotel1, hotel2) -> hotel1.rating > hotel2.rating ? hotel1 : hotel2).get().rating;
+        Optional<Hotel> cheapestBestRatedHotel = hotelList.stream()
+                .filter(h -> h.validate())
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .filter(h -> h.rating == bestRating)
+                .reduce((hotel1, hotel2) -> hotel1.weekdayRate < hotel2.weekdayRate ? hotel1 : hotel2);
+        if (cheapestBestRatedHotel.isPresent())
+            return cheapestBestRatedHotel.get();
+        return null;
+    }
+
+    public boolean validateDate(String startDate) throws InvalidInputException {
+        if (Pattern.matches("^[0-9]{4}[-][0-9]{2}[-][0-9]{2}$", startDate))
+            return true;
+        throw new InvalidInputException("Date format specified is : YYYY-MM-DD");
+    }
+
+    public boolean validateCustomerType(String customerType) throws InvalidInputException {
+        if (customerType.equalsIgnoreCase("regular") || customerType.equalsIgnoreCase("reward"))
+            return true;
+        throw new InvalidInputException("Customer type specified is: 1)Regular  2)Reward");
+    }
+
+    public Hotel findCheapestBestRatedHotelByWeekdayRatesForRewardCustomers(String startDate, String endDate) {
+        double bestRating = hotelList.stream()
+                .filter(h -> h.validate())
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .filter(h -> h.customerType.equalsIgnoreCase("reward"))
+                .reduce((hotel1, hotel2) -> hotel1.rating > hotel2.rating ? hotel1 : hotel2).get().rating;
+        Optional<Hotel> cheapestBestRatedHotel = hotelList.stream()
+                .filter(h -> h.validate())
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0).filter(h -> h.customerType.equalsIgnoreCase("reward"))
+                .filter(h -> h.rating == bestRating)
+                .reduce((hotel1, hotel2) -> hotel1.weekdayRate < hotel2.weekdayRate ? hotel1 : hotel2);
+        if (cheapestBestRatedHotel.isPresent())
+            return cheapestBestRatedHotel.get();
+        return null;
+    }
+
+}
